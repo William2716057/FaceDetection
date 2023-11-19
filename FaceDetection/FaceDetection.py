e4e9d7108967eb4fd18b63e2620d5074add42a0c
William Spriggs <Willspri@gmail.com>
2023-11-20 12:05:20 +1300

    Fixed types

--- FaceDetection/FaceDetection.py
+++ FaceDetection/FaceDetection.py
@@ -5,7 +5,7 @@ cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_
 print(cascade_path)
 
 #build classifier based on haarcascade file
-clf = vc2.CascadeClassifier(str(cascade_path)) #needs to be cast as string
+clf = cv2.CascadeClassifier(str(cascade_path)) #needs to be cast as string
 
 camera = cv2.VideoCapture(0) #should be 0 to set as computer's default
 
