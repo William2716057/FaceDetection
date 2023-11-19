40aa962609b9003d95f0c81b6e5c66b969a7d593
William Spriggs <Willspri@gmail.com>
2023-11-20 12:04:10 +1300

    updated

--- FaceDetection/FaceDetection.py
+++ FaceDetection/FaceDetection.py
@@ -4,6 +4,9 @@ import cv2
 cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
 print(cascade_path)
 
+#build classifier based on haarcascade file
+clf = vc2.CascadeClassifier(str(cascade_path)) #needs to be cast as string
 
+camera = cv2.VideoCapture(0) #should be 0 to set as computer's default
 
 
