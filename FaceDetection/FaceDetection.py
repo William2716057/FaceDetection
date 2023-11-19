8e07ab3e153e263b8c822c8805ed04606bda88e2
William Spriggs <Willspri@gmail.com>
2023-11-20 12:12:32 +1300

    updated

--- FaceDetection/FaceDetection.py
+++ FaceDetection/FaceDetection.py
@@ -9,4 +9,15 @@ clf = cv2.CascadeClassifier(str(cascade_path)) #needs to be cast as string
 
 camera = cv2.VideoCapture(0) #should be 0 to set as computer's default
 
+while True:
+    _, frame = camera.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(
+        gray, 
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
 
